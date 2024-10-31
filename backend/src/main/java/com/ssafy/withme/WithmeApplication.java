6af44d32c7d67c3e2e9dc60d7c3ff2456a9ac1e8
DESKTOP-6ABJ64L\SSAFY <shinwc1998@naver.com>
2024-10-31 13:39:53 +0900

    Refator : [임시] spring security 무시하는 어노테이션 추가

--- backend/src/main/java/com/ssafy/withme/WithmeApplication.java
+++ backend/src/main/java/com/ssafy/withme/WithmeApplication.java
@@ -3,8 +3,10 @@ package com.ssafy.withme;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//TODO : Security 구현 시 security 무시 해제
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WithmeApplication {
 
 	public static void main(String[] args) {
