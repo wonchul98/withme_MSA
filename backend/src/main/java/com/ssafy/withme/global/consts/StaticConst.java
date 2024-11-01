5c5d1e63c5fa5b5550e4d20935d24300c3b4dac1
DESKTOP-6ABJ64L\SSAFY <shinwc1998@naver.com>
2024-11-01 15:45:57 +0900

    feat : 전역 변수 설정

---
+++ backend/src/main/java/com/ssafy/withme/global/consts/StaticConst.java
@@ -0,0 +1,7 @@
+package com.ssafy.withme.global.consts;
+
+public class StaticConst {
+    /**Common*/
+    public static final int PAGEABLE_DEAFULT_PAGE = 20;
+    public static final int PAGEABLE_DEFAULT_SIZE = 0;
+}
