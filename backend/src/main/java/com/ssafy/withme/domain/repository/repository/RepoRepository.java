55a396e4f7c19e2101abcf790403f381aacd9566
DESKTOP-6ABJ64L\SSAFY <shinwc1998@naver.com>
2024-11-01 12:46:52 +0900

    refactor : 내 workspace 조회 정렬 추가

--- backend/src/main/java/com/ssafy/withme/domain/repository/repository/RepoRepository.java
+++ backend/src/main/java/com/ssafy/withme/domain/repository/repository/RepoRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface RepoRepository extends JpaRepository<Repo, Long> {
 
     Slice<Repo> findByMember_IdAndIdLessThan(Long memberId, Long cursor, Pageable pageable);
+
+    Slice<Repo> findByMember_IdAndUpdatedAtBefore(Long memberId, LocalDateTime cursor, Pageable pageable);
 }
