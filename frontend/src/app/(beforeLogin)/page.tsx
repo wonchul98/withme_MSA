cf73e4c9337d0b6bef441688620af6bf476d351a
홍범선 <qjatjs123123@naver.com>
2024-11-04 00:51:21 +0900

    feat: 랜딩페이지 초안

--- frontend/src/app/(beforeLogin)/page.tsx
+++ frontend/src/app/(beforeLogin)/page.tsx
@@ -1,8 +1,63 @@
+// app/page.tsx
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <>
-      <div className="customStyle">Hello, this text is styled with SSR-rendered inline CSS.</div>
-      <div className="s1">랜딩페이지</div>
+      <div className="container">
+        <div className="textSection">
+          <span>ReadMe 작업을</span>
+          <span>손쉽게!</span>
+        </div>
+        <div className="imageSection">
+          <Image
+            className="image"
+            src="/MainLogo.png"
+            alt="Main logo of the image"
+            width={400}
+            height={400}
+            sizes="100%"
+          />
+        </div>
+      </div>
+      <style>{`
+        .container {
+          display: flex;
+          flex-direction: row;
+          width: 100%;
+          height: calc(100vh - 67px);
+        }
+        .textSection {
+          flex: 1;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-weight: bold;
+          flex-direction: column;
+          font-size: 1.875rem; /* Tailwind의 text-3xl 크기 */
+          height: inherit;
+        }
+        .imageSection {
+          flex: 1;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: inherit;
+        }
+        .image {
+          cursor: pointer;
+          max-width: 100%;
+        }
+          @media (max-width: 768px) {
+          .container {
+            flex-direction: column;
+          }
+            .imageSection{
+            justify-content:center;
+    
+            }
+        }
+      `}</style>
     </>
   );
 }
