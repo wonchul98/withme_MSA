ce01c4d401bc9914f4a88b5a692eb3737221ba44
DESKTOP-6ABJ64L\SSAFY <shinwc1998@naver.com>
2024-11-04 13:17:19 +0900

    feat : 전역변수 수정

--- backend/src/main/java/com/ssafy/withme/global/consts/StaticConst.java
+++ backend/src/main/java/com/ssafy/withme/global/consts/StaticConst.java
@@ -2,6 +2,6 @@ package com.ssafy.withme.global.consts;
 
 public class StaticConst {
     /**Common*/
-    public static final int PAGEABLE_DEAFULT_PAGE = 20;
-    public static final int PAGEABLE_DEFAULT_SIZE = 0;
+    public static final int PAGEABLE_DEFAULT_PAGE = 0;
+    public static final int PAGEABLE_DEFAULT_SIZE = 20;
 }
