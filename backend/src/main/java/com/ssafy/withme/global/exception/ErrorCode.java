64421e123e8e31b46eaaead406030ab07458d4f6
DESKTOP-6ABJ64L\SSAFY <shinwc1998@naver.com>
2024-11-04 13:18:32 +0900

    feat : repo Error 추가

--- backend/src/main/java/com/ssafy/withme/global/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/withme/global/exception/ErrorCode.java
@@ -3,10 +3,16 @@ package com.ssafy.withme.global.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 public enum ErrorCode {
 
-    INVALID_ID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH008", "ID 토큰 검증에 실패했습니다.");
+    /**Auth*/
+    INVALID_ID_TOKEN(UNAUTHORIZED, "AUTH008", "ID 토큰 검증에 실패했습니다."),
+
+    /**Repo*/
+    REPO_NOT_FOUND(BAD_REQUEST, "REPO001", "해당 레파지토리가 존재하지 않습니다.");
 
 
     private final HttpStatus status;
