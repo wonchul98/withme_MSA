8e97d7be4a854f9da169c7d6b2a86664684532c7
gyeongmann <hkc8857@naver.com>
2024-11-05 10:38:17 +0900

    feat: Create AIDraft mock and typing print

--- frontend/src/app/edit/_components/ViewToggleButton.tsx
+++ frontend/src/app/edit/_components/ViewToggleButton.tsx
@@ -3,9 +3,9 @@ import { MarkdownView } from './MarkdownView';
 import { AIDraft } from './AIDraft';
 
 function ViewToggleButton() {
-  const [activeView, setActiveView] = useState<string>('markdown'); // Default view
+  const [activeView, setActiveView] = useState<'markdown' | 'preview' | 'ai'>('markdown');
 
-  const handleViewChange = (view: string) => {
+  const handleViewChange = (view: 'markdown' | 'preview' | 'ai') => {
     setActiveView(view);
   };
 
@@ -14,24 +14,24 @@ function ViewToggleButton() {
       <div className="flex items-center justify-center my-5 w-[300px] h-[40px] bg-[#495365] text-white rounded-[90px]">
         <button
           onClick={() => handleViewChange('markdown')}
-          className={`px-5 mx-1 text-white border-none cursor-pointer rounded-[90px] h-[30px] ${activeView === 'markdown' ? 'bg-[#49DCB0]' : 'bg-[#3D424A]'}`}
+          className={`px-5 mx-1  border-none cursor-pointer rounded-[90px] h-[30px] ${activeView === 'markdown' ? 'bg-[#49DCB0] text-black' : 'bg-[#3D424A] text-white'}`}
         >
           MarkDown
         </button>
         <button
           onClick={() => handleViewChange('preview')}
-          className={`px-5 mx-1 text-white border-none cursor-pointer rounded-[90px] h-[30px] ${activeView === 'preview' ? 'bg-[#49DCB0]' : 'bg-[#3D424A]'}`}
+          className={`px-5 mx-1 border-none cursor-pointer rounded-[90px] h-[30px] ${activeView === 'preview' ? 'bg-[#49DCB0] text-black' : 'bg-[#3D424A] text-white'}`}
         >
           Preview
         </button>
         <button
           onClick={() => handleViewChange('ai')}
-          className={`px-5 mx-1 text-white border-none cursor-pointer rounded-[90px] h-[30px] ${activeView === 'ai' ? 'bg-[#49DCB0]' : 'bg-[#3D424A]'}`}
+          className={`px-5 mx-1  border-none cursor-pointer rounded-[90px] h-[30px] ${activeView === 'ai' ? 'bg-[#49DCB0] text-black' : 'bg-[#3D424A] text-white'}`}
         >
           AI
         </button>
       </div>
-      <div className="mt-5 self-start text-[20px] w-full h-[90%] rounded-[20] p-10 bg-[#F4F4F4] overflow-x-auto overflow-y-scroll">
+      <div className="mt-5 text-[20px] w-full h-[90%] rounded-[20] p-3 bg-[#F4F4F4] overflow-x-auto overflow-y-scroll">
         {activeView === 'markdown' && <MarkdownView />}
         {activeView === 'preview' && <div>Preview Content</div>}
         {activeView === 'ai' && <AIDraft />}
