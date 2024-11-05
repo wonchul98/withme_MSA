5741b3cde23c3f8e5411fbaa4e959d6175902cdf
이재영 <ehrlrkr@gmail.com>
2024-11-05 17:18:51 +0900

    feat : changed github css from cdn to importing

--- frontend/src/app/edit/_components/MarkdownPreview.tsx
+++ frontend/src/app/edit/_components/MarkdownPreview.tsx
@@ -3,36 +3,18 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
 import rehypeRaw from 'rehype-raw';
-
-function GitHubMarkdownCSS() {
-  return (
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown.min.css"
-    />
-  );
-}
+import 'github-markdown-css';
 
 export function MarkdownPreview() {
   const { markdowns } = useMarkdown();
 
   return (
-    <div>
-      <GitHubMarkdownCSS />
-      <div className="markdown-body">
-        {markdowns?.map((markdown) => (
-          <div key={markdown.id}>
-            <ReactMarkdown
-              remarkPlugins={[remarkGfm, remarkBreaks]}
-              rehypePlugins={[rehypeRaw]}
-              // className="whitespace-pre-wrap p-5"
-              // className="p-5"
-            >
-              {markdown.content}
-            </ReactMarkdown>
-          </div>
-        ))}
-      </div>
+    <div className="markdown-body">
+      {markdowns?.map((markdown) => (
+        <ReactMarkdown key={markdown.id} remarkPlugins={[remarkGfm, remarkBreaks]} rehypePlugins={[rehypeRaw]}>
+          {markdown.content}
+        </ReactMarkdown>
+      ))}
     </div>
   );
 }
