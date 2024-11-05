17e9284db6a47fc50cbc8a260124b20cdbb0a6ac
SSAFYDKL <akhack1231@skuniv.ac.kr>
2024-11-05 10:41:50 +0900

    refactor: 레포지토리 목록을 가져오는 API의 리턴 객체 정제 및 서비스 구조화

--- backend/src/main/java/com/ssafy/withme/global/openfeign/service/APICallService.java
+++ backend/src/main/java/com/ssafy/withme/global/openfeign/service/APICallService.java
@@ -1,47 +1,21 @@
 package com.ssafy.withme.global.openfeign.service;
 
-import com.ssafy.withme.global.openfeign.FeignGithubAPIClient;
 import com.ssafy.withme.global.openfeign.dto.response.DetailResponseDTO;
 import com.ssafy.withme.global.openfeign.dto.response.RepoResponseDTO;
 import com.ssafy.withme.global.openfeign.dto.response.UserResponseDTO;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import com.ssafy.withme.global.openfeign.dto.response.refined.RefinedRepoDTO;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-@Service
-@Slf4j
-@AllArgsConstructor
-public class APICallService {
+public interface APICallService {
+    UserResponseDTO GetAuthenticatedUser(String userToken);
 
-    private final FeignGithubAPIClient feignGithubAPIClient;
+    List<RefinedRepoDTO> GetAuthenticatedUserRepos(String userToken);
 
-    public UserResponseDTO GetAuthenticatedUser(String userToken){
-        return feignGithubAPIClient.GetAuthenticatedUser(userToken);
-    }
-
-    public List<RepoResponseDTO> GetAuthenticatedUserRepos(String userToken){
-        return feignGithubAPIClient.GetAuthenticatedUserRepos(userToken);
-    }
 
     @Async
-    public CompletableFuture<List<DetailResponseDTO>> getRepoDetailsAsync(String userToken, String owner, String repo, String path) {
-        List<DetailResponseDTO> details = feignGithubAPIClient.GetRepoDetails(userToken, owner, repo, path);
-        List<DetailResponseDTO> result = new ArrayList<>(details);
-
-        // 비동기 호출로 "dir" 타입인 경우 다시 재귀적으로 호출
-        for (DetailResponseDTO detail : details) {
-            if ("dir".equals(detail.type())) {
-                result.addAll(getRepoDetailsAsync(userToken, owner, repo, detail.path()).join());
-            }else {
-                result.add(detail);
-            }
-        }
-
-        return CompletableFuture.completedFuture(result);
-    }
+    CompletableFuture<List<DetailResponseDTO>> getRepoDetailsAsync(String userToken, String owner, String repo, String path);
 }
