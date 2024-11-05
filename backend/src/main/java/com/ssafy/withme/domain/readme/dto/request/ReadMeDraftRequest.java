4cdcc5342b671341461e93eb45c6403340659a32
DESKTOP-6ABJ64L\SSAFY <shinwc1998@naver.com>
2024-11-05 16:37:25 +0900

    feat : DTO 개발

--- backend/src/main/java/com/ssafy/withme/domain/readme/dto/request/ReadMeDraftRequest.java
+++ backend/src/main/java/com/ssafy/withme/domain/readme/dto/request/ReadMeDraftRequest.java
@@ -1,8 +1,16 @@
 package com.ssafy.withme.domain.readme.dto.request;
 
-public record ReadMeDraftRequest(
-        String repository_url,
-        String section_name,
-        String user_prompt
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+public record ReadMeDraftRequest (
+        @JsonProperty("repository_url")
+        String repositoryUrl,
+        @JsonProperty("section_name")
+        String sectionName,
+        @JsonProperty("user_prompt")
+        String userPrompt
 ) {
 }
