87457ce2e8be2f1a2f8f481a36912e8f42db8826
홍범선 <qjatjs123123@naver.com>
2024-11-06 17:29:05 +0900

    feat:  버튼 가운데 트랜지션 구현

--- frontend/src/app/_components/LoginBtn.tsx
+++ frontend/src/app/_components/LoginBtn.tsx
@@ -2,32 +2,26 @@ import Link from 'next/link';
 
 export default function LoginBtn() {
   return (
-    <div className="flex  items-center h-full justify-end ">
-      <button className="login-button">
-        <Link href="/login">로그인</Link>
-      </button>
-      <style>
-        {`
+    <div className="flex items-center h-full justify-end">
+      <Link href="/login" className="login-button">
+        로그인
+      </Link>
+      <style>{`
         .login-button {
-  height: 35px;
-  border: 1px solid white;
-  color: white;
-  background-color: transparent;
-  border-radius: 30px;
-  padding: 4px 16px; /* px-4, py-1 */
-  transition: background-color 300ms, color 300ms;
-}
+          height: 35px;
+          border: 1px solid white;
+          color: white;
+          background-color: transparent;
+          border-radius: 30px;
+          padding: 4px 16px; /* px-4, py-1 */
+          transition: background-color 300ms, color 300ms;
+        }
 
-.login-button:hover {
-  background-color: white;
-  color: black;
-}
-
-.justify-between {
-  justify-content: space-between;
-}
-        `}
-      </style>
+        .login-button:hover {
+          background-color: white;
+          color: black;
+        }
+      `}</style>
     </div>
   );
 }
--- frontend/src/app/edit/_components/FoldButton.tsx
+++ frontend/src/app/edit/_components/FoldButton.tsx
@@ -1,8 +1,11 @@
 import { FaChevronLeft } from 'react-icons/fa';
 
-export default function FoldButton() {
+export default function FoldButton({ toggleSidebar }) {
   return (
-    <button className="absolute top-[400px] left-[319px] h-[60px] w-[20px] rounded-r-lg bg-gray-900">
+    <button
+      onClick={toggleSidebar}
+      className={`absolute top-[calc(50%-30px)] left-60 h-[60px] w-[20px] rounded-r-lg bg-gray-900 transform -translate-y-1/2`}
+    >
       <FaChevronLeft color="white" />
     </button>
   );
