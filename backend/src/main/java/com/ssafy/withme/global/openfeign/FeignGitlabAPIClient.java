23f9adcc86500b26ca18fb84197535b71e56f056
taegun1011 <xorjsghkd1011@gmail.com>
2024-11-07 19:37:40 +0900

    Feat: add gitlab api client

---
+++ backend/src/main/java/com/ssafy/withme/global/openfeign/FeignGitlabAPIClient.java
@@ -0,0 +1,24 @@
+package com.ssafy.withme.global.openfeign;
+
+import com.ssafy.withme.global.openfeign.dto.response.gitlab.GLDetailResponseDTO;
+import com.ssafy.withme.global.openfeign.dto.response.gitlab.GLRepoResponseDTO;
+import com.ssafy.withme.global.openfeign.dto.response.gitlab.GLUserResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
+
+@FeignClient(name="GitlabAPI", url="https://lab.ssafy.com/api/v4", configuration = FeignGitlabConfig.class)
+public interface FeignGitlabAPIClient {
+
+    @GetMapping("/user")
+    GLUserResponseDTO GetAuthenticatedUser(@RequestHeader("Authorization") String userToken);
+
+    @GetMapping("/projects?min_access_level=30&membership=true&search=S11")
+    List<GLRepoResponseDTO> GetAuthenticatedUserRepos(@RequestHeader("Authorization") String userToken);
+
+    @GetMapping("/repos/{owner}/{repo}/contents/{path}")
+    List<GLDetailResponseDTO> GetRepoDetails(@RequestHeader("Authorization") String userToken, @PathVariable String owner, @PathVariable String repo, @PathVariable String path);
+}
