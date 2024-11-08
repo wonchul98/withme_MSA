75084811fc0ebbf76ba928e989b5c12e2179cd3e
gyeongmann <hkc8857@naver.com>
2024-11-08 15:57:38 +0900

    chore: delete empty line of config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,15 +2,13 @@
 const config = {
   reactStrictMode: true,
   images: {
-
-
     domains: [
       'liveblocks.io',
       'dummyimage.com',
       'picsum.photos',
       'hongboemsunimage.s3.ap-northeast-2.amazonaws.com',
       'avatars.githubusercontent.com',
-      'withme.s3.amazonaws.com'
+      'withme.s3.amazonaws.com',
     ],
   },
   typescript: {
