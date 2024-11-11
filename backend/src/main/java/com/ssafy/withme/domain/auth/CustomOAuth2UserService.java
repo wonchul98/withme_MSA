01411d3173339254c6b9d643c410e17f57765554
taegun1011 <xorjsghkd1011@gmail.com>
2024-11-11 14:52:54 +0900

    Chores: minor change to trigger webhook

--- backend/src/main/java/com/ssafy/withme/domain/auth/CustomOAuth2UserService.java
+++ backend/src/main/java/com/ssafy/withme/domain/auth/CustomOAuth2UserService.java
@@ -9,7 +9,6 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
