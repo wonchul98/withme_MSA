2e2aa734d68e9fce28ad329f639e896147d13f19
홍범선 <qjatjs123123@naver.com>
2024-11-12 12:27:11 +0900

    fix: invisible 재 호출

--- frontend/src/app/(afterLogin)/workspace/_component/CreateBtn.tsx
+++ frontend/src/app/(afterLogin)/workspace/_component/CreateBtn.tsx
@@ -3,20 +3,22 @@ import { useGlobalState } from '../../_components/RepoModalProvider';
 import { API_URL, MESSAGE } from '@/util/constants';
 import { useQueryClient } from '@tanstack/react-query';
 import useErrorHandler from '../business/useErrorHandler';
+import { useUserRepoQuery } from '@/stores/server/getUserRepoQuery';
 
 export default function CreateBtn({ url }) {
   const { curRepo, setCurRepo } = useGlobalState();
   const queryClient = useQueryClient();
   const { setIsVisible } = useGlobalState();
   const { handlerAxios } = useErrorHandler();
+  const { refetch: repoRefetch } = useUserRepoQuery(null);
 
   const handleCreate = async () => {
     setIsVisible(false);
     await handlerAxios(
       async () => await axios.post(`${API_URL.CREATE_WORKSPACE}`, { workspace_id: curRepo.current.id }),
-      () => {
+      async () => {
         queryClient.invalidateQueries({ queryKey: ['workspace'] });
-        queryClient.invalidateQueries({ queryKey: ['userReopKey'] });
+        await repoRefetch();
       },
       MESSAGE.REPO_CREATE,
       MESSAGE.REPO_SUCCESS,
