9df970cf3a796cb3f6361aa3588af591f160be95
taegun1011 <xorjsghkd1011@gmail.com>
2024-11-12 14:47:48 +0900

    Feat: implement elasticsearch

--- backend/src/main/java/com/ssafy/withme/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/withme/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -84,6 +85,15 @@ public class SecurityConfig {
     public WebSecurityCustomizer configureH2ConsoleEnable() {
         return web -> web.ignoring()
                 .requestMatchers(PathRequest.toH2Console());
+    }
+
+    @Bean
+    @Profile("local")
+    public SecurityFilterChain localSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                                .anyRequest().permitAll());
+        return http.build();
     }
 
     @Bean
