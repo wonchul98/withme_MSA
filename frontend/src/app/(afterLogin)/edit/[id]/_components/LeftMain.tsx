45d4a21f8106e3deb021c5c1d20b157d5aa7fe42
gyeongmann <hkc8857@naver.com>
2024-11-12 13:16:08 +0900

    fix: Fix Editor enter Error

--- frontend/src/app/(afterLogin)/edit/[id]/_components/LeftMain.tsx
+++ frontend/src/app/(afterLogin)/edit/[id]/_components/LeftMain.tsx
@@ -3,7 +3,12 @@ import { useMenuItems } from '../_contexts/MenuItemsContext';
 import { RoomProvider, ClientSideSuspense } from '@liveblocks/react/suspense';
 import { Editor } from './Editor';
 
-export default function LeftMain() {
+interface LeftMainProps {
+  connected: Set<string>;
+  setConnected: React.Dispatch<React.SetStateAction<Set<string>>>;
+}
+
+export default function LeftMain({ connected, setConnected }: LeftMainProps) {
   const { initialItems } = useMenuItems();
 
   if (!initialItems || initialItems.length === 0) {
@@ -14,14 +19,22 @@ export default function LeftMain() {
     <ClientSideSuspense fallback={<div></div>}>
       <div className="bg-white h-full w-full">
         {initialItems.map((item) => (
-          <RoomWithEditor key={`room-${item.id}`} id={item.id} />
+          <RoomWithEditor key={`room-${item.id}`} id={item.id} connected={connected} setConnected={setConnected} />
         ))}
       </div>
     </ClientSideSuspense>
   );
 }
 
-function RoomWithEditor({ id }: { id: string }) {
+function RoomWithEditor({
+  id,
+  connected,
+  setConnected,
+}: {
+  id: string;
+  connected: Set<string>;
+  setConnected: React.Dispatch<React.SetStateAction<Set<string>>>;
+}) {
   const { activeId } = useActiveId();
   return (
     <RoomProvider
@@ -33,7 +46,7 @@ function RoomWithEditor({ id }: { id: string }) {
     >
       <ClientSideSuspense fallback={<div>test3...</div>}>
         <div className={`p-4 transition-opacity duration-300 ${activeId === id ? '' : 'hidden pointer-events-none'}`}>
-          <Editor />
+          <Editor connected={connected} setConnected={setConnected} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
