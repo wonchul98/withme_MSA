955caf6384244ddb6cc5ff6d1f8cdeb6ee2e16d3
taegun1011 <xorjsghkd1011@gmail.com>
2024-11-13 20:18:51 +0900

    Feat: add 'owner' and some field on WorkspaceInfo response DTO

--- backend/src/main/java/com/ssafy/withme/domain/workspace/dto/Response/WorkspaceInfoResponse.java
+++ backend/src/main/java/com/ssafy/withme/domain/workspace/dto/Response/WorkspaceInfoResponse.java
@@ -5,17 +5,22 @@ import com.ssafy.withme.domain.repository.entity.Repo;
 import com.ssafy.withme.domain.workspace.entity.Workspace;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 import static com.ssafy.withme.global.consts.StaticConst.WORKSPACE_ROOM_ID_PREFIX;
 
 public record WorkspaceInfoResponse (
     Long id,
     String name,
+    String owner,
     String thumbnail,
     String repoUrl,
     Boolean isCreated,
     String readmeContent,
     Boolean isPrivate,
-    String roomId
+    String roomId,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
 ){
     @Builder
     public WorkspaceInfoResponse{
--- backend/src/main/java/com/ssafy/withme/domain/workspace/dto/Response/WorkspaceSimpleInfoResponse.java
+++ backend/src/main/java/com/ssafy/withme/domain/workspace/dto/Response/WorkspaceSimpleInfoResponse.java
@@ -4,11 +4,16 @@ package com.ssafy.withme.domain.workspace.dto.Response;
 import com.ssafy.withme.domain.workspace.entity.Workspace;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 public record WorkspaceSimpleInfoResponse(
         String name,
+        String owner,
         String thumbnail,
         String repoUrl,
-        String readmeContent
+        String readmeContent,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
 ){
     @Builder
     public WorkspaceSimpleInfoResponse{
@@ -17,9 +22,12 @@ public record WorkspaceSimpleInfoResponse(
     public static WorkspaceSimpleInfoResponse from(Workspace workspace) {
         return WorkspaceSimpleInfoResponse.builder()
                 .name(workspace.getName())
+                .owner(workspace.getOwner())
                 .thumbnail(workspace.getThumbnail())
                 .repoUrl(workspace.getRepoUrl())
                 .readmeContent(workspace.getReadmeContent())
+                .createdAt(workspace.getCreatedAt())
+                .updatedAt(workspace.getUpdatedAt())
                 .build();
     }
 }
