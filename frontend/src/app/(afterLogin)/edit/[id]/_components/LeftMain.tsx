d8448d6e8aefa54209ddc3603fb5b6da6dd104a8
gyeongmann <hkc8857@naver.com>
2024-11-15 01:21:58 +0900

    feat: show current menu editor and preview

--- frontend/src/app/(afterLogin)/edit/[id]/_components/LeftMain.tsx
+++ frontend/src/app/(afterLogin)/edit/[id]/_components/LeftMain.tsx
@@ -23,6 +23,8 @@ export default function LeftMain() {
 
 function RoomWithEditor({ id }: { id: string }) {
   const { activeId } = useActiveId();
+  const { menuItems } = useMenuItems();
+  const activeMenu = menuItems.find((menu) => menu.id === activeId);
   return (
     <RoomProvider
       id={`room-${id}`}
@@ -35,6 +37,7 @@ function RoomWithEditor({ id }: { id: string }) {
         <div
           className={`p-4 transition-opacity duration-300 h-full ${activeId === id ? '' : 'hidden pointer-events-none'}`}
         >
+          <div className="text-xl ml-[53px] pb-2 border-b-2 mb-2">{activeMenu.label}</div>
           <Editor />
         </div>
       </ClientSideSuspense>
