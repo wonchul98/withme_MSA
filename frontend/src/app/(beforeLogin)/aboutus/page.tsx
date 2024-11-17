6efce98438c784c313c40bb1330d0ae9671424bd
홍범선 <qjatjs123123@naver.com>
2024-11-18 00:18:42 +0900

    feat: 이미지 import 후 적용

--- frontend/src/app/(beforeLogin)/aboutus/page.tsx
+++ frontend/src/app/(beforeLogin)/aboutus/page.tsx
@@ -1,9 +1,16 @@
 import Footer from '@/app/_components/Footer';
 import Image from 'next/image';
 
+import EditImage from '/public/edit.png';
+import PreviewMarkdownImage from '/public/preview&markdown.png';
+import AIDraftImage from '/public/AIDraft.png';
+import TabGroupImage from '/public/tab_group.png';
+import Capture1Image from '/public/capture1.png';
+import Capture2Image from '/public/capture2.png';
+
 export default function AboutUs() {
   return (
-    <div className="responsive_aboutResponsive ">
+    <div className="responsive_aboutResponsive">
       <div className="flex aboutus-responsive-image justify-between my-[100px] w-full gap-[100px]">
         <div className="flex flex-col h-100 mr-[100px]">
           <span className="text-5xl">
@@ -16,10 +23,10 @@ export default function AboutUs() {
             and ask AI to add the finishing touches.
           </span>
         </div>
-        <div className="">
+        <div>
           <Image
             className="image border-2 border-gray-200 rounded-xl"
-            src="/edit.png"
+            src={EditImage}
             alt="Editor full view"
             width={800}
             height={600}
@@ -32,10 +39,10 @@ export default function AboutUs() {
       <div className="w-full border-t-2 border-gray-2"></div>
 
       <div className="flex aboutus-responsive-image justify-between my-[100px] w-full gap-[100px]">
-        <div className="">
+        <div>
           <Image
-            className="image border-4 border-gray-200 rounded-xl aboutImage "
-            src="/preview&markdown.png"
+            className="image border-4 border-gray-200 rounded-xl aboutImage"
+            src={PreviewMarkdownImage}
             alt="preview&markdown"
             width={800}
             height={600}
@@ -43,8 +50,8 @@ export default function AboutUs() {
             sizes="100%"
           />
         </div>
-        <div className="flex flex-col h-100 text-end ">
-          <span className="text-5xl ">
+        <div className="flex flex-col h-100 text-end">
+          <span className="text-5xl">
             Real-time Preview <br />
             and Markdown.
           </span>{' '}
@@ -73,7 +80,7 @@ export default function AboutUs() {
         <div>
           <Image
             className="image border-2 border-gray-200 rounded-xl"
-            src="/AIDraft.png"
+            src={AIDraftImage}
             alt="AIDraft"
             width={800}
             height={600}
@@ -88,8 +95,8 @@ export default function AboutUs() {
       <div className="flex aboutus-responsive-image justify-between my-[100px] w-full gap-[100px]">
         <div>
           <Image
-            className="image  "
-            src="/tab_group.png"
+            className="image"
+            src={TabGroupImage}
             alt="tab_group"
             width={800}
             height={600}
@@ -124,10 +131,10 @@ export default function AboutUs() {
             git social login
           </span>
         </div>
-        <div className="">
+        <div>
           <Image
             className="image border-2 border-gray-200 rounded-xl"
-            src="/capture1.PNG"
+            src={Capture1Image}
             alt="capture of the Editor image"
             width={800}
             height={600}
@@ -140,10 +147,10 @@ export default function AboutUs() {
       <div className="w-full border-t-2 border-gray-2"></div>
 
       <div className="flex aboutus-responsive-image justify-between my-[100px] w-full gap-[100px]">
-        <div className="">
+        <div>
           <Image
             className="image border-4 border-gray-200 rounded-xl"
-            src="/capture2.PNG"
+            src={Capture2Image}
             alt="capture of the Editor image"
             width={800}
             height={600}
@@ -153,13 +160,13 @@ export default function AboutUs() {
         </div>
         <div className="flex flex-col h-100 text-end">
           <span className="text-5xl">
-            Find everthing <br />
+            Find everything <br />
             Get Insight
           </span>{' '}
           <br />
           <span className="text-xl text-[#ccc]">
             No more endless searching. <br />
-            Our built in Elastic Search Skill finds what you are <br />
+            Our built-in Elastic Search Skill finds what you are <br />
             looking for in our apps
           </span>
         </div>
