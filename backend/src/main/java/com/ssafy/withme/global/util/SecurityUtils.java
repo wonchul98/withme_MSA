551ba15138cac1e6dad6b4420490354e5fd86189
wonchul98 <shinwc1998@naver.com>
2025-02-21 11:57:23 +0900

    Refactor : 불필요한 import 문 제거

--- backend/src/main/java/com/ssafy/withme/global/util/SecurityUtils.java
+++ backend/src/main/java/com/ssafy/withme/global/util/SecurityUtils.java
@@ -7,10 +7,6 @@ import com.ssafy.withme.global.exception.BusinessException;
 import com.ssafy.withme.global.exception.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Component
